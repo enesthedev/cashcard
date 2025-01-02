0984cdc4bc70bdef8b3c61c71a80a2c667b35079
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2025-01-02 02:54:28 +0100

    feat: add failing test

---
+++ src/test/java/com/enesthedev/cashcard/CashCardJsonTest.java
@@ -0,0 +1,12 @@
+package com.enesthedev.cashcard;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CashCardJsonTest {
+    @Test
+    void failingTest() {
+        assertThat(1).isEqualTo(42);
+    }
+}
