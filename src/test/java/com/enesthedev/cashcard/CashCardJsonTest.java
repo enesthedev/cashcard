30eb9a87e8a18679ea95994aee202c384af90873
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2025-01-02 03:11:13 +0100

    feat: add cash card serialization tests

--- src/test/java/com/enesthedev/cashcard/CashCardJsonTest.java
+++ src/test/java/com/enesthedev/cashcard/CashCardJsonTest.java
@@ -1,12 +1,27 @@
 package com.enesthedev.cashcard;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@JsonTest
 class CashCardJsonTest {
+    @Autowired
+    private JacksonTester<CashCard> json;
+
     @Test
-    void failingTest() {
-        assertThat(1).isEqualTo(42);
+    void testSerialize() throws IOException {
+        CashCard card = new CashCard(1L, 100.0);
+
+        assertThat(json.write(card)).isStrictlyEqualToJson("cashcard.expected.json");
+        assertThat(json.write(card)).hasJsonPathNumberValue("@.id");
+        assertThat(json.write(card)).extractingJsonPathNumberValue("@.id").isEqualTo(1);
+        assertThat(json.write(card)).hasJsonPathNumberValue("@.amount");
+        assertThat(json.write(card)).extractingJsonPathNumberValue("@.amount").isEqualTo(100.0);
     }
 }
