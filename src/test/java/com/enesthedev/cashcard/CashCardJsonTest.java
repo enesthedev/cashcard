64ca385191f1b7b29be960e5a42c68938cb65613
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2025-01-02 03:17:09 +0100

    feat: add deserialize test

--- src/test/java/com/enesthedev/cashcard/CashCardJsonTest.java
+++ src/test/java/com/enesthedev/cashcard/CashCardJsonTest.java
@@ -1,5 +1,6 @@
 package com.enesthedev.cashcard;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
@@ -14,6 +15,8 @@ class CashCardJsonTest {
     @Autowired
     private JacksonTester<CashCard> json;
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Test
     void testSerialize() throws IOException {
         CashCard card = new CashCard(1L, 100.0);
@@ -23,5 +26,18 @@ class CashCardJsonTest {
         assertThat(json.write(card)).extractingJsonPathNumberValue("@.id").isEqualTo(1);
         assertThat(json.write(card)).hasJsonPathNumberValue("@.amount");
         assertThat(json.write(card)).extractingJsonPathNumberValue("@.amount").isEqualTo(100.0);
+    }
+
+    @Test
+    void testDeserialize() throws IOException {
+        CashCard expectedCard = new CashCard(1L, 100.0);
+        CashCard card = objectMapper.readValue(
+                getClass().getResourceAsStream("cashcard.expected.json"), // Ensure file is in resources
+                CashCard.class
+        );
+
+        assertThat(card).isNotNull();
+        assertThat(card.id()).isEqualTo(expectedCard.id());
+        assertThat(card.amount()).isEqualTo(expectedCard.amount());
     }
 }
